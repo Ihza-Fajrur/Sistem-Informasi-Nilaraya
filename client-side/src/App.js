6da8971944cefdc50475e80368c707440c52d1ac
mayonice1424 <friyandrailham14@gmail.com>
2022-04-12 16:25:54 +0700

    Menambahkan background

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,5 +1,6 @@
 import Background from "./components/background_login/Background";
 import React from "react";
+import "./app.scss";
 function App() {
   return (
     <div>
--- client-side/src/components/background_login/Background.jsx
+++ client-side/src/components/background_login/Background.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import "./background.scss";
 export default function Background() {
   return (
     <div className="background-containt">
       <img
-        className="background"
         src={`${process.env.PUBLIC_URL}/Background.png`}
         alt="background"
+        height="100%"
+        width="100%"
+        position="fixed"
       />
     </div>
   );
