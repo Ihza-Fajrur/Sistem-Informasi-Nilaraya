c65cad045dc8591427882ec8ce2bed0fb592bbe9
mayonice1424 <friyandrailham14@gmail.com>
2022-04-26 12:25:18 +0700

    mengedit table gigi

--- client-side/src/pages/Admin/RekamMedis/FormTambahRekamMedis/FormGigi.jsx
+++ client-side/src/pages/Admin/RekamMedis/FormTambahRekamMedis/FormGigi.jsx
@@ -145,7 +145,7 @@ export default function FormUmum() {
             <thead>
               <thead>
                 <tr>
-                  <th scope="col">
+                  <th className="checkbox" scope="col">
                     <label>Check All</label>
                     <input
                       type="checkbox"
